9e563bb1d655a04a138c054a8b4a98799d93745a
schopra <sm237512@dal.ca>
2023-06-28 18:55:35 -0300

    final

--- trello-frontend/src/Pages/SignUp.js
+++ trello-frontend/src/Pages/SignUp.js
@@ -5,79 +5,91 @@ import { LoadingButton } from "@mui/lab";
 import {
   Container,Stack,TextField,Box,Typography} from "@mui/material";
 
+
 // import VisibilityIcon from "@mui/icons-material/Visibility";
 // import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
-export default function Register() {
+export default function SignUp() {
   const [passShow, setPass] = useState(false);
 
-  const RegisterSchema = Yup.object().shape({
-    firstName: Yup.string()
-      .min(2, "Too Short!")
-      .max(50, "Too Long!")
-      .required("First name required"),
-    lastName: Yup.string()
-      .min(2, "Too Short!")
-      .max(50, "Too Long!")
-      .required("Last name required"),
+  const Register = Yup.object().shape({
+    fName: Yup.string()
+       // in case first name is not entered
+      .required("Enter first name"),
+      lName: Yup.string()
+      // in case last name is not entered
+      .required("Enter last name"),
     email: Yup.string()
-      .email("Email must be a valid email address")
-      .required("Email is required"),
-    password: Yup.string().required("Password is required"),
-    passwordConfirmation: Yup.string().oneOf(
+      .email("not valid!")
+      .required("Email is required!"),
+    password: Yup.string().required("Password is required!"),
+    confirmation: Yup.string().oneOf(
       [Yup.ref("password"), null],
-      "Passwords must match"
+      "Passwords are not matching!!"
     ),
   });
 
+
+
   const formik = useFormik({
     initialValues: {
-      firstName: '',
-      lastName: '',
+      fName: '',
+      lName: '',
       email: '',
       password: '',
-      passwordConfirmation: '',
+      confirmation: '',
     },
-    validationSchema: RegisterSchema,
-    // onSubmit: async (values) => {
-    //   const { emailID, pass, fname, lname } = values;
-    //   console.log(emailID, pass, fname, lname);
-    // },
+    validationSchema: Register,
+
+    onSubmit: async (values) => {
+      // const { email, password, firstName, lastName } = values;
+      console.log(values);
+
+   
+    
+      
+
+    },
   });
+
+
+  
 
   const { errors, touched, isSubmitting, handleSubmit, getFieldProps } = formik;
 
   return (
-    <Container  sx={{ height: "80%" }}>
-      <Box sx={{ mt: 20 }}>
-        <Stack spacing={5}>
+    <Container >
+      <Box >
+        <Stack >
           <Box>
             <Typography
-              variant="h3"
+              variant="h1"
               sx={{
-                textAlign: "center",
+                textAlign: "right",
               }}
             >
               Sign up page - Enter your details
             </Typography>
           </Box>
+          
           <FormikProvider value={formik}>
-            <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
-              <Stack spacing={3}>
+            <Form onSubmit={handleSubmit}>
+            
+              <Stack >
                 <TextField
                   fullWidth
                   label="First name"
-                  {...getFieldProps("firstName")}
-                  error={Boolean(touched.firstName && errors.firstName)}
-                  helperText={touched.firstName && errors.firstName}
+                  {...getFieldProps("fName")}
+                  error={Boolean(touched.fName && errors.fName)}
+                  helperText={touched.fName && errors.fName}
                 />
 
                 <TextField
                   fullWidth
                   label="Last name"
-                  {...getFieldProps("lastName")}
-                  error={Boolean(touched.lastName && errors.lastName)}
-                  helperText={touched.lastName && errors.lastName}
+                  {...getFieldProps("lName")}
+                  error={Boolean(touched.lName && errors.lName)}
+                  helperText={touched.lName && errors.lName}
                 />
 
                 <TextField
@@ -104,22 +116,7 @@ export default function Register() {
                   type={passShow ? "text" : "password"}
                   label="Confirm Password"
                   {...getFieldProps("passwordConfirmation")}
-                //   InputProps={{
-                //     endAdornment: (
-                //       <InputAdornment position="end">
-                //         <IconButton
-                //           edge="end"
-                //           onClick={() => setPass((prev) => !prev)}
-                //         >
-                //           {passShow ? (
-                //             <VisibilityIcon />
-                //           ) : (
-                //             <VisibilityOffIcon />
-                //           )}
-                //         </IconButton>
-                //       </InputAdornment>
-                //     ),
-                //   }}
+                
                   error={Boolean(
                     touched.passwordConfirmation && errors.passwordConfirmation
                   )}
