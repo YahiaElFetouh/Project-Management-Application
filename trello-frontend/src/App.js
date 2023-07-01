9a110a6ff56335a7867c964d1e418aa61bb77a87
schopra <sm237512@dal.ca>
2023-07-01 17:47:00 -0300

    temporary

--- trello-frontend/src/App.js
+++ trello-frontend/src/App.js
@@ -4,11 +4,14 @@ import{ store } from "./store"
 import {BrowserRouter} from "react-router-dom";
 import Router from "./routes";
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
     <>
     <Provider store={store}>
+      <ToastContainer />
       <BrowserRouter>
       <Router />
       
