85b36ae83d70f1d6a9dcab52836eb088c9c321c0
Ahmed Galal <a.galal@dal.ca>
2023-07-01 21:57:37 -0300

    Merged Yahia's Branche by Copy + Paste

--- Deliverables/Deliverable-2/trello-frontend/src/Pages/LoginForm.js
+++ Deliverables/Deliverable-2/trello-frontend/src/Pages/LoginForm.js
@@ -1,76 +1,94 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import qs from 'qs'; 
-import '../layouts/LoginForm.css'
+import qs from 'qs';
+import './LoginForm.css'
+import ForgotPassword from './ForgotPassword';
 const LoginForm = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [showForgotPassword, setShowForgotPassword] = useState(false);
 
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
-  };
+    const handleForgotPasswordClick = () => {
+        setShowForgotPassword(true);
+    };
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
+    const handleLogin = () => {
+        setShowForgotPassword(false);
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    
-    console.log('username:', username);
-    console.log('password:', password);
-    const data = qs.stringify({
-      email: username,
-      password: password
-    });
-   
-    
-    axios.post('http://127.0.0.1:8080/UserSignIn/login', data)
-      .then(response => {
-         const data = response.data;
-         if (data.code === 200) {
-          alert("Login Successful, Welcome!")
+    if (showForgotPassword) {
+        return <ForgotPassword handleLogin={handleLogin} />;
+    }
 
-         }else{
-          alert("Username or Password invalid!")
-         }
-      })
-      .catch(error => {
-        console.log('loginfail', error);
-        alert("Error");
-        
-      });
-  };
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
+    };
 
-  return (
-   <form onSubmit={handleSubmit}>
-      <div class="login-container">
-      <div class="login-form">
-        <label for="username"></label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          placeholder='email'
-          onChange={handleUsernameChange}
-        />
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
 
-        <label for="password"></label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          placeholder='password'
-          onChange={handlePasswordChange}
-        />
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-         <button type="submit">Login</button>
-      </div>
-       </div>
-      
-    </form>
-    
-  );
+        console.log('username:', username);
+        console.log('password:', password);
+        const data = qs.stringify({
+            email: username,
+            password: password
+        });
+
+
+        axios.post('http://127.0.0.1:8080/UserSignIn/login', data)
+            .then(response => {
+                const data = response.data;
+                if (data.code === 200) {
+                    alert("Login Successful, Welcome!")
+
+                }else{
+                    alert("Username or Password invalid!")
+                }
+            })
+            .catch(error => {
+                console.log('loginfail', error);
+                alert("Error");
+
+            });
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div class="login-container">
+                <div class="login-form">
+                    <label for="username"></label>
+                    <input
+                        type="text"
+                        id="username"
+                        value={username}
+                        placeholder='email'
+                        onChange={handleUsernameChange}
+                    />
+
+                    <label for="password"></label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        placeholder='password'
+                        onChange={handlePasswordChange}
+                    />
+
+                    <button type="submit">Login</button>
+                    <p>
+                        <button onClick={handleForgotPasswordClick}>Forgot Password?</button>
+                    </p>
+                </div>
+            </div>
+
+        </form>
+
+    );
 };
+
 
 export default LoginForm;
