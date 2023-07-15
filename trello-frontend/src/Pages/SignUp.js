bed39e45290f79f928c117e7386450f33fb00500
schopra <sm237512@dal.ca>
2023-07-15 17:16:07 -0300

    changed structure and added code

--- trello-frontend/src/Pages/SignUp.js
+++ trello-frontend/src/Pages/SignUp.js
@@ -1,129 +1,116 @@
 import * as Yup from "yup";
-import { useState } from "react";
-import { Form, FormikProvider, useFormik } from "formik";
-import { LoadingButton } from "@mui/lab";
-import {
-  Container,Stack,TextField,Box,Typography} from "@mui/material";
-  import { useNavigate } from "react-router-dom";
-  import { useDispatch } from "react-redux";
+import {useState } from "react";
+import {Box, Button, Container, IconButton, InputAdornment, Stack, TextField, Typography} from "@mui/material";
+import {Form, FormikProvider, useFormik} from "formik";
+import {LoadingButton} from "@mui/lab";
+
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import { useDispatch } from "react-redux";
+import { setUser } from "../store/slices/UserSlice";
+import { useNavigate } from "react-router-dom";
 
 
-export default function SignUp() {
-  const [passShow, setPass] = useState(false);
 
-  const Register = Yup.object().shape({
-    fName: Yup.string()
-       // in case first name is not entered
-      .required("Enter first name"),
-      lName: Yup.string()
-      // in case last name is not entered
-      .required("Enter last name"),
-    email: Yup.string()
-      .email("not valid!")
-      .required("Email is required!"),
-    securityAnswer: Yup.string()
-      // in case last name is not entered
-      .required("Enter Answer"),
-    password: Yup.string().required("Password is required!"),
-    confirmation: Yup.string().oneOf(
-      [Yup.ref("password"), null],
-      "Passwords are not matching!!"
-    ),
-  });
+export default function Register() {
 
-  // const dispatch = useDispatch();
+    const [showPassword, setShowPassword] = useState(false);
+
+    const RegisterSchema = Yup.object().shape({
+        // firstName: Yup.string()
+        //     .min(2, "Too Short!")
+        //     .max(50, "Too Long!")
+        //     .required("First name required"),
+        // lastName: Yup.string()
+        //     .min(2, "Too Short!")
+        //     .max(50, "Too Long!")
+        //     .required("Last name required"),
+        email: Yup.string()
+            .email("Email must be a valid email address")
+            .required("Email is required"),
+        password: Yup.string()
+            .required('Password is required')
+            .min(8, "Too Short!")
+            .matches(
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()])/,
+                'Password must contain at least one lowercase letter, one uppercase letter, one number, and one symbol'
+            ),
+        passwordConfirmation: Yup.string().oneOf(
+            [Yup.ref("password"), null],
+            "Passwords must match"),
+        securityAnswer: Yup.string()
+            .required("securityAnswer is required"),
+    });
+
+    const dispatch = useDispatch();
     const navigate = useNavigate();
 
 
 
-   const formik = useFormik({
-  initialValues: {
-    fName: '',
-    lName: '',
-    email: '',
-    password: '',
-    confirmation: '',
-    securityAnswer: '',
-  },
-  validationSchema: Register,
 
-  onSubmit: async (values) => {
-    const {email, password, fName, lName, securityAnswer} = values;
-    console.log(values);
+    const formik = useFormik({
+        initialValues: {
+           
+            email: '',
+            password: '',
+            passwordConfirmation: '',
+            securityAnswer: '',
+        },
+        validationSchema: RegisterSchema,
+        onSubmit: async (values) => {
+            const {email, password,  securityAnswer} = values;
+            console.log(email,password)
+            console.log("hi")
+            console.log(values);
 
+            dispatch(
+                setUser({
+                    email,
+                    password,
+                    securityAnswer,
+                })
+            );
 
-  // Code for linking the frontend and the backend:
-  // Referred these sources:
-  //1) https://dmitripavlutin.com/javascript-fetch-async-await/
+            console.log('dwjdnw')
 
-  try {
-      await fetch('http://localhost:8080/users/save', {
-          method: 'POST',
-          body: JSON.stringify({
-              fName,
-              lName,
-              email,
-              password,
-              securityAnswer,
-          }),
-          headers: {
-              'Content-Type': 'application/json',
-          },
-      });
+            try {
+              fetch('http://localhost:8080/User/save', {
+                 method: 'POST',
+                 body: JSON.stringify({
+                     email,
+                     password,
+                     securityAnswer,
+                 }),
+                 headers: {
+                     'Content-Type': 'application/json',
+                 },
+             });
+     
+             // Registration successful, navigate to the login page
+            //  navigate("/login");
+         } catch (error) {
+             console.error(error);
+             // Handle error if the registration request fails
+         }
 
-      // Registration successful, navigate to the login page
-      navigate("/login");
-  } catch (error) {
-      console.error(error);
-      // Handle error if the registration request fails
-  }
-},
-});
+         console.log("hehe nhi hua")
 
+            
+        },
+    });
 
-
-  
-
-  const { errors, touched, isSubmitting, handleSubmit, getFieldProps } = formik;
+    const { errors, touched, isSubmitting, handleSubmit, getFieldProps } = formik;
 
   return (
-    <Container >
-      <Box >
-        <Stack >
-          <Box>
-            <Typography
-              variant="h1"
-              sx={{
-                textAlign: "right",
-              }}
-            >
-              Sign up page - Enter your details
-            </Typography>
-          </Box>
-          
+    <Container maxWidth="sm" sx={{ height: "100%" }}>
+        <Stack spacing={5}>
           <FormikProvider value={formik}>
-            <Form onSubmit={handleSubmit}>
-            
-              <Stack >
+            <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
+              <Stack spacing={3}>
+                
                 <TextField
                   fullWidth
-                  label="First name"
-                  {...getFieldProps("fName")}
-                  error={Boolean(touched.fName && errors.fName)}
-                  helperText={touched.fName && errors.fName}
-                />
-
-                <TextField
-                  fullWidth
-                  label="Last name"
-                  {...getFieldProps("lName")}
-                  error={Boolean(touched.lName && errors.lName)}
-                  helperText={touched.lName && errors.lName}
-                />
-
-                <TextField
-                  fullWidth
-                  autoComplete="username"
+                  autoComplete="email"
                   type="email"
                   label="Email address"
                   {...getFieldProps("email")}
@@ -142,10 +129,25 @@ export default function SignUp() {
 
                 <TextField
                   fullWidth
-                  type={passShow ? "text" : "password"}
+                  type={showPassword ? "text" : "password"}
                   label="Confirm Password"
                   {...getFieldProps("passwordConfirmation")}
-                
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          edge="end"
+                          onClick={() => setShowPassword((prev) => !prev)}
+                        >
+                          {showPassword ? (
+                            <VisibilityIcon />
+                          ) : (
+                            <VisibilityOffIcon />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
                   error={Boolean(
                     touched.passwordConfirmation && errors.passwordConfirmation
                   )}
@@ -153,26 +155,31 @@ export default function SignUp() {
                     touched.passwordConfirmation && errors.passwordConfirmation
                   }
                 />
-                <TextField
-                            fullWidth
-                            label="Answer"
-                            {...getFieldProps("securityAnswer"
-                            )}
-                            error={Boolean(touched.securityAnswer && errors.securityAnswer)}
-                            helperText={touched.securityAnswer && errors.securityAnswer}
-                        />
 
+<TextField
+                  fullWidth
+                  autoComplete="securityAnswer"
+                  
+                  label="security Answer"
+                  {...getFieldProps("securityAnswer")}
+                  error={Boolean(touched.securityAnswer && errors.securityAnswer)}
+                  helperText={touched.securityAnswer && errors.securityAnswer}
+                />
 
-                
-
-                <LoadingButton loading={isSubmitting}>
-                  Sign Up
+                <LoadingButton
+                  fullWidth
+                  size="large"
+                  type="submit"
+                  variant="contained"
+                  loading={isSubmitting}
+                >
+                  Register
                 </LoadingButton>
               </Stack>
             </Form>
           </FormikProvider>
         </Stack>
-      </Box>
     </Container>
   );
 }
+
